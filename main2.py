5d4b97bdabea871b263f205c2d8d81a85a1e214f
JakesCode <jakestr1999@gmail.com>
2015-01-28 21:01:41 +0000

    QuestCode Version 1.4
    - JOURNAL updated
    - USE function added
    - More storyline added

--- main2.py
+++ main2.py
@@ -5,7 +5,7 @@ itemDict = {"blank": "There is nothing here!", "map": "A crumpled and worn map. 
 
 journalDict = {"letter": "Hello there! I hear you're about to take the Memory Loss Potion. So, I welcome you to your new life. Enjoy the sights you see! This should be the only thing you'll have on you, if you have anything else - I highly recommend you drop it now. You might even remember who you are! Lots of luck - Dr. Brandshire (CEO of NewLife Corp)"}
 
-fullJournalDict = {"letter": "Hello there! I hear you're about to take the Memory Loss Potion. So, I welcome you to your new life. Enjoy the sights you see! This should be the only thing you'll have on you, if you have anything else - I highly recommend you drop it now. You might even remember who you are! Lots of luck - Dr. Brandshire (CEO of NewLife Corp)", "wrench": "The wrench I found seems to have a few red marks on it. I can't say it's blood, but it looks too dark to be paint. I'll have to remember this for future reference."}
+fullJournalDict = {"letter": "Hello there! I hear you're about to take the Memory Loss Potion. So, I welcome you to your new life. Enjoy the sights you see! This should be the only thing you'll have on you, if you have anything else - I highly recommend you drop it now. You might even remember who you are! Lots of luck - Dr. Brandshire (CEO of NewLife Corp)", "wrench": "The wrench I found seems to have a few red marks on it. I can't say it's blood, but it looks too dark to be paint. I'll have to remember this for future reference.", "glass": "This piece of glass was with many other pieces of glass. This is the largest piece, it seems.", "bolts": "There are bolts around the door, near where I found the broken glass. Perhaps a certain tool will help me open the door.", "poison": "The storage locker in the Pub seemed to have a little too much Rat Poison to match the cleanliness of the Pub. Something is going on here...."}
 
 #Set up all the processes and functions etc.
 #----------------------------------------------------------------
@@ -47,6 +47,9 @@ def mainProcess(command):
 	if command=="journal":
 		displayMessage("journal")
 		pass
+	if command=="use":
+		displayMessage("use")
+		pass
 
 
 def inspectItem(item):
@@ -77,6 +80,46 @@ def callEvent(eventName):
 		journalData.append("wrench")
 		journalDict["wrench"] = fullJournalDict["wrench"]
 		storyProgression += 10
+		pass
+
+	if eventName=="glass" and storyProgression==10:
+		os.system("cls")
+		print("Wait.... this isn't the only piece. There's loads of glass here....")
+		print("Could this possibly be a murder case?")
+		print("")
+		print("There seems to be a door near the glass, but it's bolted shut.")
+		print("")
+		print("! - Journal Entry Added")
+		journalData.append("glass")
+		journalDict["glass"] = fullJournalDict["glass"]
+		storyProgression += 10
+		journalData.append("bolts")
+		journalDict["bolts"] = fullJournalDict["bolts"]
+		pass
+
+def useItem(item):
+	global storyProgression
+	global currentLoc
+
+	if item=="wrench" and currentLoc==2 and storyProgression==20:
+		os.system("cls")
+		print("The door is unbolted.")
+		print("")
+		print("It's just a storage locker. There's a few screws and around four bottles.")
+		print("The bottles seem to all be rat poison. I don't see why somebody would need")
+		print("such an exessive amount; the pub seems clean enough....")
+		print("")
+		print("! - Journal Entry Added")
+		print("")
+		input("Please press a key....")
+		os.system("cls")
+		print("! - Mystery Completed! - 'The Bolted Door'")
+		input("Press any key to return....")
+		journalData.append("poison")
+		journalDict["poison"] = fullJournalDict["poison"]
+		storyProgression += 10
+		journalData.remove("bolts")
+		pass
 
 def takeItem(passedItem):
 	playerInv.append(locItems[currentLoc])
@@ -85,6 +128,10 @@ def takeItem(passedItem):
 	# Is it an event item?
 	if itemData[currentLoc]=="wrench":
 		callEvent("wrench")
+		pass
+
+	if itemData[currentLoc]=="glass":
+		callEvent("glass")
 		pass
 
 	locItems[currentLoc]=""
@@ -108,6 +155,7 @@ def displayMessage(call):
 		print("- SCAN - View the items in the current location.")
 		print("- TAKE - Allows the player to obtain an item in a certain location.")
 		print("- JOURNAL - Read thoughts of the player and re-read certain events.")
+		print("- USE - Apply an item to an area. This will lead to the completion of mysteries.")
 		print("")
 		print("Believe it or not, there's a story in this game.")
 		print("Your current position in the story is indicated by a percentage")
@@ -199,6 +247,22 @@ def displayMessage(call):
 		print("")
 		entrySelection = input("?: ")
 		journalRead(entrySelection)
+		pass
+
+	if call=="use":
+		os.system("cls")
+		print("Use which item?")
+		print("")
+		print(playerInv)
+		print("")
+		print("Their keywords are as follows:")
+		print("")
+		print(inventoryData)
+		print("")
+		print("Please enter the keyword you want:")
+		print("")
+		useSelection = input("?: ")
+		useItem(useSelection)
 		pass
 
 	print("")
